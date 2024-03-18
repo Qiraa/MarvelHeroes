2f520db50d1fb4117ad157593c7df5c802df487a
Minger Ekaterina <minger.ekaterina@gmail.com>
2024-03-18 22:20:51 +0600

    Create package for components

--- app/src/main/java/com/example/marvelsuperheroes/ui/components/MainCard.kt
+++ app/src/main/java/com/example/marvelsuperheroes/ui/components/MainCard.kt
@@ -1,4 +1,4 @@
-package com.example.marvelsuperheroes.ui.screens
+package com.example.marvelsuperheroes.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
